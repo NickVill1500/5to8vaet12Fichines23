90a6cc81df18d6f6c348dcabc878f03588625c8d
sara <sara1330castro@gmail.com>
2023-12-13 10:40:47 -0300

    Pendrive Update 02

--- Fichines.Dapper/AdoDapper.cs
+++ Fichines.Dapper/AdoDapper.cs
@@ -1,5 +1,6 @@
 using System.Data;
 using Dapper;
+using Fichines.core.Dapper;
 using Fichines.core.Tablas;
 using MySqlConnector;
 
--- Fichines.TestDapper/TestAdo.cs
+++ Fichines.TestDapper/TestAdo.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Fichines.core;
+using Fichines.core.Dapper;
 using Xunit;
 
 namespace Fichines.TestDapper
